d4cec2343aafb72e06efa8c92618048e8c7669ed
golovchenkovalery <techno041016@gmail.com>
2022-12-04 16:14:41 +0200

    ne

---
+++ app/src/main/java/com/example/weatherappcursey/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.weatherappcursey
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.weatherappcursey.fragments.MainFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportFragmentManager
+            .beginTransaction()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.placeHolder, MainFragment.newInstance())
+            .commit()
+    }
+}
